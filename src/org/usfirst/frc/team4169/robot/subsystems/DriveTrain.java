92ecd4e3b2d21874538fd66ad8e3be3216420472
Calvin Terpstra <calvinterpstra@gmail.com>
2015-05-05 13:39:12 -0400

    started making DriveTrain subsystem

---
+++ src/org/usfirst/frc/team4169/robot/subsystems/DriveTrain.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team4169.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.*;
+
+/**
+ *
+ */
+public class DriveTrain extends Subsystem {
+	
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    
+}
+
