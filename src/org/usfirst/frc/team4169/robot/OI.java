9d6fcf662d1e1630ca634ce900569f0e8b685fcf
Jack Doherty <jack_doherty@team4169.com>
2015-05-05 13:10:54 -0400

    test commit undo

--- src/org/usfirst/frc/team4169/robot/OI.java
+++ src/org/usfirst/frc/team4169/robot/OI.java
@@ -2,8 +2,7 @@ package org.usfirst.frc.team4169.robot;
 
 import edu.wpi.first.wpilibj.buttons.Button;
 import org.usfirst.frc.team4169.robot.commands.ExampleCommand;
-//test
-//test 2
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
