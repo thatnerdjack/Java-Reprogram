7e6a6069b959e38e5b4a4a5adf383cbcc3046f14
Jack Doherty <jack_doherty@team4169.com>
2015-05-07 14:37:07 -0400

    cleaned up DriveTrain subsystem, moved fields into RobotMap

--- src/org/usfirst/frc/team4169/robot/RobotMap.java
+++ src/org/usfirst/frc/team4169/robot/RobotMap.java
@@ -1,4 +1,9 @@
 package org.usfirst.frc.team4169.robot;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -6,13 +11,26 @@ package org.usfirst.frc.team4169.robot;
  * floating around.
  */
 public class RobotMap {
-    // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-    // public static int leftMotor = 1;
-    // public static int rightMotor = 2;
+    public static Talon DRIVE_FRONT_LEFT_MOTOR;
+    public static Talon DRIVE_FRONT_RIGHT_MOTOR;
+    public static Talon DRIVE_BACK_LEFT_MOTOR;
+    public static Talon DRIVE_BACK_RIGHT_MOTOR;
     
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
+    public static RobotDrive DRIVE;
+    
+    public static void init() {
+    	DRIVE_FRONT_LEFT_MOTOR = new Talon(12);
+    	LiveWindow.addActuator("DriveTrain", "Motor FL", (Talon) DRIVE_FRONT_LEFT_MOTOR);
+    	
+		DRIVE_FRONT_RIGHT_MOTOR = new Talon(3);
+		LiveWindow.addActuator("DriveTrain", "Motor FL", (Talon) DRIVE_FRONT_RIGHT_MOTOR);
+		
+		DRIVE_BACK_LEFT_MOTOR = new Talon(13);
+		LiveWindow.addActuator("DriveTrain", "Motor FL", (Talon) DRIVE_BACK_LEFT_MOTOR);
+		
+		DRIVE_BACK_RIGHT_MOTOR = new Talon(2);
+		LiveWindow.addActuator("DriveTrain", "Motor FL", (Talon) DRIVE_BACK_RIGHT_MOTOR);
+		
+		DRIVE = new RobotDrive(DRIVE_FRONT_LEFT_MOTOR, DRIVE_BACK_LEFT_MOTOR, DRIVE_FRONT_RIGHT_MOTOR, DRIVE_BACK_RIGHT_MOTOR);
+	}
 }
--- src/org/usfirst/frc/team4169/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team4169/robot/subsystems/DriveTrain.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team4169.robot.subsystems;
 
+import org.usfirst.frc.team4169.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.*;
@@ -8,29 +10,16 @@ import edu.wpi.first.wpilibj.*;
  *
  */
 public class DriveTrain extends Subsystem {
-	private SpeedController motorFL;
-	private SpeedController motorFR;
-	private SpeedController motorBL;
-	private SpeedController motorBR;
-	
-	private RobotDrive drive;
-	
-	public DriveTrain() {
-		motorFL = new Talon(12);
-		motorFR = new Talon(3);
-		motorBL = new Talon(13);
-		motorBR = new Talon(2);
-		
-		drive = new RobotDrive(motorFL, motorBL, motorFR, motorBR);
-		
-		LiveWindow.addActuator("DriveTrain", "Motor FL", (Talon) motorFL);
-		LiveWindow.addActuator("DriveTrain", "Motor FR", (Talon) motorFR);
-		LiveWindow.addActuator("DriveTrain", "Motor BL", (Talon) motorBL);
-		LiveWindow.addActuator("DriveTrain", "Motor BR", (Talon) motorBR);
-	}
+	private RobotDrive drive = RobotMap.DRIVE;
 	
 	public void drive() {
 		drive.drive(1, 0);
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
 	}
     
 }
