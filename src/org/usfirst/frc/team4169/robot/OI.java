e8c2be77e2cb257d152efd3487d23feebeeaf30c
Calvin Terpstra <calvinterpstra@gmail.com>
2015-05-11 12:57:54 -0400

    finished up the OI and the drive command

--- src/org/usfirst/frc/team4169/robot/OI.java
+++ src/org/usfirst/frc/team4169/robot/OI.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team4169.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 import org.usfirst.frc.team4169.robot.commands.ExampleCommand;
 
@@ -10,34 +11,52 @@ import org.usfirst.frc.team4169.robot.commands.ExampleCommand;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
-    
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
-    
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-    
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-    
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-    
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
-
-	Joystick stick = new Joystick(1);
+	
+	private Joystick joy = new Joystick(0);
+	
+	public OI() {
+	    //// CREATING BUTTONS
+	    // One type of button is a joystick button which is any button on a joystick.
+	    // You create one by telling it which joystick it's on and which button
+	    // number it is.
+	    // Joystick stick = new Joystick(port);
+	    // Button button = new JoystickButton(stick, buttonNumber);
+	    
+	    // There are a few additional built in buttons you can use. Additionally,
+	    // by subclassing Button you can create custom triggers and bind those to
+	    // commands the same as any other Button.
+	    
+	    //// TRIGGERING COMMANDS WITH BUTTONS
+	    // Once you have a button, it's trivial to bind it to a button in one of
+	    // three ways:
+	    
+	    // Start the command when the button is pressed and let it run the command
+	    // until it is finished as determined by it's isFinished method.
+	    // button.whenPressed(new ExampleCommand());
+	    
+	    // Run the command while the button is being held down and interrupt it once
+	    // the button is released.
+	    // button.whileHeld(new ExampleCommand());
+	    
+	    // Start the command when the button is released  and let it run the command
+	    // until it is finished as determined by it's isFinished method.
+	    // button.whenReleased(new ExampleCommand());
+			
+		JoystickButton d_up = new JoystickButton(joy, 5);
+	    JoystickButton d_right= new JoystickButton(joy, 6);
+	    JoystickButton d_down= new JoystickButton(joy, 7);
+	    JoystickButton d_left = new JoystickButton(joy, 8);
+	    JoystickButton l2 = new JoystickButton(joy, 9);
+	    JoystickButton r2 = new JoystickButton(joy, 10);
+	    JoystickButton l1 = new JoystickButton(joy, 11);
+	    JoystickButton r1 = new JoystickButton(joy, 12);
+	    
+	    
+	}
+	    
+    public Joystick getJoystick() {
+        return joy;
+    }
 	
 }
 
--- src/org/usfirst/frc/team4169/robot/commands/MecanumDriveWithController.java
+++ src/org/usfirst/frc/team4169/robot/commands/MecanumDriveWithController.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team4169.robot.commands;
 
+import org.usfirst.frc.team4169.robot.Robot;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -11,15 +13,17 @@ public class MecanumDriveWithController extends Command {
     public MecanumDriveWithController() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(Robot.driveTrain);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	stick = new Joystick(joystickChannel);
+    	
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.driveTrain.mechDrive(Robot.oi.getJoystick());
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -29,10 +33,12 @@ public class MecanumDriveWithController extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.driveTrain.mechDrive(0, 0, 0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
