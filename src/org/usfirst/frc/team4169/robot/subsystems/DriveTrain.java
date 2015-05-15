546a6b84cbbdb57b773ac402193dc2f8de9ee569
Jack Doherty <jack_doherty@team4169.com>
2015-05-15 14:38:59 -0400

    worked on stuff

--- src/org/usfirst/frc/team4169/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team4169/robot/subsystems/DriveTrain.java
@@ -4,7 +4,6 @@ import org.usfirst.frc.team4169.robot.RobotMap;
 import org.usfirst.frc.team4169.robot.commands.MecanumDriveWithController;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.*;
 
 /**
@@ -19,7 +18,7 @@ public class DriveTrain extends Subsystem {
 	}
 	
 	public void mechDrive(Joystick j) {
-		mechDrive(j.getX(), j.getY(), j.getTwist());
+		mechDrive(j.getX(), j.getY(), 0);
 	}
 	
 	public void mechDrive(double x, double y, double rotation) {
